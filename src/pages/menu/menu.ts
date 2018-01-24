02c47e0e18a9f2736daeee966f7e6c128a7284e9
Mohammed Zaki <mohammedzaki.dev@gmail.com>
2018-01-24 13:16:19 +0200

    update menu style

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, Nav, NavController } from 'ionic-angular';
-import { HomePage, DevicesPage, DeviceHistoryPage, AboutPage } from "../pages";
+import { IonicPage, Nav, ModalController } from 'ionic-angular';
+import { HomePage, DevicesPage, DeviceHistoryPage, AboutPage, LoginPage, NotificationModal } from "../pages";
 
 export interface PageInterface {
     title: string;
@@ -23,7 +23,7 @@ export class MenuPage {
 
     pages: PageInterface[];
 
-    constructor(public navCtrl: NavController) {
+    constructor(private modalCtrl:ModalController) {
 
         // used for an example of ngFor and navigation
         this.pages = [
@@ -31,10 +31,12 @@ export class MenuPage {
             { title: 'DEVICES', pageName: 'DevicesPage', component: DevicesPage, index: 1, icon: 'ios-cart' },
             { title: 'HISTORY', pageName: 'DeviceHistoryPage', component: DeviceHistoryPage, index: 2, icon: 'contacts' },
             { title: 'ABOUT US', pageName: 'AboutPage', component: AboutPage, index: 3, icon: 'information-circle' },
-            { title: 'LOGOUT', pageName: 'LOGOUT', component: AboutPage, index: 4, icon: 'information-circle' }
+            { title: 'NOTIFICATIONS', pageName: 'NotificationModal', component: NotificationModal, index: 4, icon: 'information-circle' },
+            { title: 'LOGOUT', pageName: 'LoginPage', component: LoginPage, index: 5, icon: 'information-circle' }
         ];
 
     }
+
 
     openPage(page: PageInterface) {
         let params = {};
@@ -43,8 +45,11 @@ export class MenuPage {
             params = { tabIndex: page.index }
         }
 
-        if (this.nav.getActiveChildNavs()[ 0 ] && page.index != undefined) {
+        /*if (this.nav.getActiveChildNavs()[ 0 ] && page.index != undefined) {
             this.nav.getActiveChildNavs()[ 0 ].select(page.index);
+        } else*/ if (page.index == 4) {
+            let modal = this.modalCtrl.create(NotificationModal);
+            modal.present();
         } else {
             this.nav.setRoot(page.component, params);
         }
@@ -52,19 +57,21 @@ export class MenuPage {
 
     isActive(page: PageInterface) {
         // Again the Tabs Navigation
-        let childNav = this.nav.getActiveChildNavs()[ 0 ];
+        /*let childNav = this.nav.getActiveChildNavs()[ 0 ];
 
         if (childNav) {
             if (childNav.getSelected() && childNav.getSelected().root === page.component) {
-                return 'primary';
+                return 'mainRedColor';
             }
             return;
         }
 
         // Fallback needed when there is no active childnav (tabs not active)
         if (this.nav.getActive() && this.nav.getActive().name === page.pageName) {
-            return 'primary';
-        }
-        return;
+            return 'mainRedColor';
+        }*/
+        return 'mainBlueColor';
     }
+
+
 }
